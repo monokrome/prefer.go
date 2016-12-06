903a0dc4d17efa7c65baa16506a4bd1a6154e394
Bailey Stoner <monokrome@monokro.me>
2016-12-06 11:49:16 -0800

    Add XML encoding.

--- serializers.go
+++ serializers.go
@@ -1,24 +1,38 @@
 package prefer
 
-import "encoding/json"
+import (
+	"encoding/json"
+	"encoding/xml"
+)
 
+// NOTE: It may make more sense to use a map to these instead of creating
+// potentially unnecessray structs for implementing interfaces on.
 type Serializer interface {
-	Serialize(obj interface{}) ([]byte, error)
-	Deserialize(data []byte, obj interface{}) error
+	Serialize(interface{}) ([]byte, error)
+	Deserialize([]byte, interface{}) error
 }
 
 func NewSerializer(identifier string) (serializer Serializer, err error) {
 	// TODO: Automatic discovery of which loader type to use
-	serializer = JSONSerializer{}
-	return serializer, err
+	return JSONSerializer{}, err
 }
 
 type JSONSerializer struct{}
 
-func (serializer JSONSerializer) Serialize(obj interface{}) ([]byte, error) {
-	return json.Marshal(obj)
+func (this JSONSerializer) Serialize(input interface{}) ([]byte, error) {
+	return json.Marshal(input)
 }
 
-func (serializer JSONSerializer) Deserialize(data []byte, obj interface{}) error {
-	return json.Unmarshal(data, &obj)
+func (this JSONSerializer) Deserialize(input []byte, obj interface{}) error {
+	return json.Unmarshal(input, &obj)
+}
+
+type XMLSerializer struct{}
+
+func (this XMLSerializer) Serialize(input interface{}) ([]byte, error) {
+	return xml.Marshal(input)
+}
+
+func (this XMLSerializer) Deserialize(input []byte, obj interface{}) error {
+	return xml.Unmarshal(input, &obj)
 }
---
+++ serializers_test.go
@@ -0,0 +1,57 @@
+package prefer
+
+import "testing"
+
+const (
+	MOCK_NAME  = "Mock Name"
+	MOCK_VALUE = 12
+)
+
+type MockSubject struct {
+	Name  string
+	Value int
+}
+
+func check(t *testing.T, err error) {
+	if err != nil {
+		t.Error(err)
+	}
+}
+
+func TestJSONSerializer(t *testing.T) {
+	serializer := JSONSerializer{}
+
+	subject := MockSubject{
+		Name:  MOCK_NAME,
+		Value: MOCK_VALUE,
+	}
+
+	serialized, err := serializer.Serialize(subject)
+	check(t, err)
+
+	result := MockSubject{}
+	check(t, serializer.Deserialize(serialized, &result))
+
+	if result != subject {
+		t.Error("Result does not match original serialized object.")
+	}
+}
+
+func TestXMLSerializer(t *testing.T) {
+	serializer := XMLSerializer{}
+
+	subject := MockSubject{
+		Name:  MOCK_NAME,
+		Value: MOCK_VALUE,
+	}
+
+	serialized, err := serializer.Serialize(subject)
+	check(t, err)
+
+	result := MockSubject{}
+	check(t, serializer.Deserialize(serialized, &result))
+
+	if result != subject {
+		t.Error("Result does not match original serialized object.")
+	}
+}
